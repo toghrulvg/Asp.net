003bca197f384e994836cea5c7241a0cf38304fb
Toghrul V. Guluzada <toghrulvg@code.local>
2022-09-30 12:09:25 +0400

    added

---
+++ Task/WebApplication1/Contorller/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Contorller
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Yapram()
+        {
+            return  "tamam yaprak";
+        }
+
+    }
+}
